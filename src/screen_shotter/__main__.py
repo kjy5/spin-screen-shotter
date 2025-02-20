9725d0357491db8bab676fb54b7c42aa66b53730
Kenneth Yang <kjy5@uw.edu>
2025-02-19 22:42:32 -0800

    Rotate

--- src/screen_shotter/__main__.py
+++ src/screen_shotter/__main__.py
@@ -1,9 +1,22 @@
+from time import sleep
 import pyautogui as pg
 from pyautogui import Point
-from time import sleep
 
 screen_size = pg.size()
 center = Point(screen_size.width // 2, screen_size.height // 2)
 
-if __name__ == '__main__':
-    print(center)
+PREFIX = "closer"
+OUTPUT_DIR = "output"
+
+if __name__ == "__main__":
+    pg.moveTo(center)
+    pg.click()
+    
+    sleep(1)
+    
+    for i in range(15):
+        pg.moveTo(center)
+        with pg.hold("shift"):
+            pg.drag(200, 0, duration=0.25, button="left")
+
+        pg.screenshot(f"{OUTPUT_DIR}/{PREFIX}_{i}.png")
